e1032b0e99b47e6e6fa8de1e805c5b89deb59e03
Hrvoje <hvidakovic13@gmail.com>
2023-05-09 13:22:26 +0200

    inital commit

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Knjiznica")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("72e17fa9-e1a6-4d4c-a201-7663096084e3")]
+[assembly: Guid("c076620a-d301-4173-8950-05786b941007")]
 
 // Version information for an assembly consists of the following four values:
 //
