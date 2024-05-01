cebf245fc81e0f3114a56c7a86116052aa5cd126
Hrvoje <hvidakovic13@gmail.com>
2024-05-01 22:36:17 +0200

    Dodao podatkovni kontkst

---
+++ Knjiznica/model/PodatkovniKontekst.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knjiznica.model
+{
+    internal class PodatkovniKontekst
+    {
+        // Kolekcije 
+        public List<Ucenik> Ucenici;
+        public List<Knjiga> Knjige;
+        public List<Posudba> Posudbe;
+        // Ostale članove ove klase dodat ćemo kasnijs
+    }
+}
