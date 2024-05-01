86505a23f164afc3943e9b9ae423e9a484e6a1cf
Hrvoje <hvidakovic13@gmail.com>
2024-05-01 23:10:04 +0200

    Dodao formu za ucenike

---
+++ Knjiznica/Forme/Ucenici.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Knjiznica.Forme
+{
+    partial class Ucenici
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBucenici = new System.Windows.Forms.ListBox();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnUredi = new System.Windows.Forms.Button();
+            this.btnObrisi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LBucenici
+            // 
+            this.LBucenici.FormattingEnabled = true;
+            this.LBucenici.Location = new System.Drawing.Point(28, 46);
+            this.LBucenici.Name = "LBucenici";
+            this.LBucenici.Size = new System.Drawing.Size(372, 186);
+            this.LBucenici.TabIndex = 0;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(435, 46);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(106, 40);
+            this.btnDodaj.TabIndex = 1;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // btnUredi
+            // 
+            this.btnUredi.Location = new System.Drawing.Point(435, 121);
+            this.btnUredi.Name = "btnUredi";
+            this.btnUredi.Size = new System.Drawing.Size(106, 40);
+            this.btnUredi.TabIndex = 2;
+            this.btnUredi.Text = "Uredi";
+            this.btnUredi.UseVisualStyleBackColor = true;
+            this.btnUredi.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnObrisi
+            // 
+            this.btnObrisi.Location = new System.Drawing.Point(435, 194);
+            this.btnObrisi.Name = "btnObrisi";
+            this.btnObrisi.Size = new System.Drawing.Size(106, 40);
+            this.btnObrisi.TabIndex = 3;
+            this.btnObrisi.Text = "Obriši";
+            this.btnObrisi.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Učenici";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Ucenici
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 304);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnObrisi);
+            this.Controls.Add(this.btnUredi);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.LBucenici);
+            this.Name = "Ucenici";
+            this.Text = "Ucenici";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LBucenici;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnUredi;
+        private System.Windows.Forms.Button btnObrisi;
+        private System.Windows.Forms.Label label1;
+    }
+}
