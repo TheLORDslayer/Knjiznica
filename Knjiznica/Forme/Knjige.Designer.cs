8fbc325cd6e7f59dfe38262adae8d71594d8607b
Hrvoje <hvidakovic13@gmail.com>
2024-05-02 22:11:29 +0200

    Dodao formu za knjigu

---
+++ Knjiznica/Forme/Knjige.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Knjiznica.Forme
+{
+    partial class Knjige
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBknjige = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnUredi = new System.Windows.Forms.Button();
+            this.btnObrisi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LBknjige
+            // 
+            this.LBknjige.FormattingEnabled = true;
+            this.LBknjige.Location = new System.Drawing.Point(26, 59);
+            this.LBknjige.Name = "LBknjige";
+            this.LBknjige.Size = new System.Drawing.Size(451, 212);
+            this.LBknjige.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Knjige";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(551, 59);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(112, 36);
+            this.btnDodaj.TabIndex = 3;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // btnUredi
+            // 
+            this.btnUredi.Location = new System.Drawing.Point(551, 128);
+            this.btnUredi.Name = "btnUredi";
+            this.btnUredi.Size = new System.Drawing.Size(112, 36);
+            this.btnUredi.TabIndex = 4;
+            this.btnUredi.Text = "Uredi";
+            this.btnUredi.UseVisualStyleBackColor = true;
+            // 
+            // btnObrisi
+            // 
+            this.btnObrisi.Location = new System.Drawing.Point(551, 193);
+            this.btnObrisi.Name = "btnObrisi";
+            this.btnObrisi.Size = new System.Drawing.Size(112, 36);
+            this.btnObrisi.TabIndex = 5;
+            this.btnObrisi.Text = "Obriši";
+            this.btnObrisi.UseVisualStyleBackColor = true;
+            // 
+            // Knjige
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 377);
+            this.Controls.Add(this.btnObrisi);
+            this.Controls.Add(this.btnUredi);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LBknjige);
+            this.Name = "Knjige";
+            this.Text = "Knjige";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LBknjige;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnUredi;
+        private System.Windows.Forms.Button btnObrisi;
+    }
+}
